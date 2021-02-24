5e9e90a0409448dd0082c58044d5a0116aa9406d
wu <1191125750@qq.com>
2021-02-24 14:22:33 +0800

    change

--- src/main.js
+++ src/main.js
@@ -14,38 +14,38 @@ Vue.prototype._i18n = i18n
 Vue.prototype.$echarts = echarts
 
 const i18n = new VueI18n({
-  locale: datastorage.getSync({key:'langMsg'}) ? datastorage.getSync({key:'langMsg'}).name : 'en-US',
-  messages: {
-    'en-US': lang.en,
-    'zh-CN': lang.cn,
-    "ko-KR":lang.kr,
-    'ja_JP':lang.jp
-  },
-  silentTranslationWarn: true,
+    locale: datastorage.getSync({key: 'langMsg'}) ? datastorage.getSync({key: 'langMsg'}).name : 'en-US',
+    messages: {
+        'en-US': lang.en,
+        'zh-CN': lang.cn,
+        "ko-KR": lang.kr,
+        'ja_JP': lang.jp
+    },
+    silentTranslationWarn: true,
 })
 
 const setHtmlFontSize = () => {
 
-  const htmlDom = document.getElementsByTagName('html')[0];
+    const htmlDom = document.getElementsByTagName('html')[0];
 
-  let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
-  // console.log(document.documentElement.clientWidth)
-  // console.log(document.body.clientWidth)
+    let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    // console.log(document.documentElement.clientWidth)
+    // console.log(document.body.clientWidth)
 
-  if (htmlWidth >= 1920) {
-    htmlWidth = 1920;
-  }
-  if (htmlWidth <= 320) {
-    htmlWidth = 320;
-  }
-  //1rem = 1px
-  htmlDom.style.fontSize = `${(htmlWidth / 1920)}px`;
+    if (htmlWidth >= 1920) {
+        htmlWidth = 1920;
+    }
+    if (htmlWidth <= 320) {
+        htmlWidth = 320;
+    }
+    //1rem = 1px
+    htmlDom.style.fontSize = `${(htmlWidth / 1920)}px`;
 };
 window.onresize = setHtmlFontSize;
 setHtmlFontSize();
 
 new Vue({
-  i18n,
-  router,
-  render: h => h(App)
+    i18n,
+    router,
+    render: h => h(App)
 }).$mount('#app')
