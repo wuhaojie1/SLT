0cd2cb278f19c976f972a8d8b297b853bb3145e4
wu <1191125750@qq.com>
2021-02-27 18:14:48 +0800

    change

--- src/views/buy/buy.vue
+++ src/views/buy/buy.vue
@@ -1,17 +1,29 @@
 <template>
     <div id="buy">
-        <router-view>
-
-        </router-view>
+        <tradebar></tradebar>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import tradebar from "../../components/tradebar/tradebar";
     export default {
         name: "buy",
+        data(){
+          return{
+
+          }
+        },
+        components:{
+            tradebar
+        }
     }
 </script>
 
 <style scoped lang="less">
+    #buy{
+        width: 100%;
+        height: 100%;
+    }
 
 </style>
