16026049d0b3a8f1cc5dd8429a3f86c91c7d59c8
coderybh <2046230119@qq.com>
2021-02-28 23:09:55 +0800

    change

---
+++ src/components/AddressTable/AddressTable.vue
@@ -0,0 +1,169 @@
+<template>
+    <div id="atable">
+        <table>
+            <thead>
+            <tr>
+                <th>
+                    <div class="man">收货人</div>
+                </th>
+                <th>
+                    <div class="local">所在地区</div>
+                </th>
+                <th>
+                    <div class="adress">详细地址</div>
+                </th>
+                <th>
+                    <div class="price">邮编</div>
+                </th>
+                <th>
+                    <div class="phone">电话/手机</div>
+                </th>
+                <th>
+                    <div class="oprate">操作</div>
+                </th>
+                <th>
+                    <div class="setadress"></div>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th>
+                    <div class="man-con">李歪歪</div>
+                </th>
+                <th>
+                    <div class="local-con">
+                        <div class="address-text">四川省 成都市 双流区 华阳镇街道</div>
+                    </div>
+                </th>
+                <th>
+                    <div class="adress-con">瑞升 橡树林华府五单元1401</div>
+                </th>
+                <th>
+                    <div class="price-con">000000</div>
+                </th>
+                <th>
+                    <div class="phone-con">86-13528143556</div>
+                </th>
+                <th>
+                    <div class="oprate-con">修改|删除</div>
+                </th>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AddressTable"
+    }
+</script>
+
+<style scoped lang="less">
+#atable{
+    width: 100%;
+    height: 100%;
+
+    table {
+        width: 100%;
+        border: 1rem solid #E4E7ED;
+        font-size: 14rem;
+        font-family: Source Han Sans CN;
+        font-weight: 400;
+        color: #444444;
+        thead {
+            border-bottom: 1rem solid #E4E7ED;
+            background: #EBECF0;
+            height: 42rem;
+            tr {
+                border: 1rem solid #E4E7ED;
+                th {
+                    text-align: left;
+                    height: 42rem;
+                    .man{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 83rem;
+                    }
+                    .local{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 135rem;
+                    }
+                    .adress{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 228rem;
+                    }
+                    .price{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 117rem;
+                    }
+                    .phone{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 160rem;
+                    }
+                    .oprate{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 129rem;
+                    }
+                    .setadress{
+                        width: 132rem;
+                    }
+                }
+            }
+        }
+        tbody{
+            tr{
+                text-align: left;
+                height: 74rem;
+                border-bottom: 1rem solid #E4E7ED;
+                th{
+                    .man-con{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 83rem;
+                    }
+                    .local-con{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 135rem;
+                    }
+                    .adress-con{
+                        margin-left: 20rem;
+                        /*line-height: 42rem;*/
+                        width: 228rem;
+                        .address-text{
+                            width: 90rem;
+                            height: auto;
+                            margin: 0 auto;
+                        }
+                    }
+                    .price-con{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 117rem;
+                    }
+                    .phone-con{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 160rem;
+                    }
+                    .oprate-con{
+                        margin-left: 20rem;
+                        line-height: 42rem;
+                        width: 129rem;
+                    }
+                    .setadress-con{
+                        width: 132rem;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
--- src/views/user/userInfo.vue
+++ src/views/user/userInfo.vue
@@ -16,16 +16,76 @@
                 <div class="userinfo-name-text">真实姓名:</div>
                 <input type="text" class="userinfo-name-input" :value="name">
             </div>
+            <div class="user-sex-con">
+                <div class="user-sex-text">性别:</div>
+                <div class="user-sex-choice">
+<!--                    <div class="user-sex-item">-->
+<!--                        <input type="radio" value="男">-->
+<!--                    </div>-->
+<!--                    <div class="user-sex-item">-->
+<!--                        <input type="radio" value="女">-->
+<!--                    </div>-->
+<!--                    <div class="user-sex-item">-->
+<!--                        <input type="radio" value="保密">-->
+<!--                    </div>-->
+                    <el-radio-group v-model="radio">
+                        <el-radio :label="3" class="user-sex-item"><span :style="{marginLeft:'11rem'}">男</span></el-radio>
+                        <el-radio :label="6" class="user-sex-item"><span :style="{marginLeft:'11rem'}">女</span></el-radio>
+                        <el-radio :label="9" class="user-sex-item"><span :style="{marginLeft:'11rem'}">保密</span></el-radio>
+                    </el-radio-group>
+                </div>
+                <div class="user-bri-con">
+                    <div class="userinfo-bri-text">生日:</div>
+                    <div class="set-data-con">
+                        <dataselect></dataselect>
+                    </div>
+                </div>
+                <div class="user-stu-con">
+                    <div class="userinfo-stu-text">教育程度:</div>
+                    <div class="set-stu-con">
+                        <select  placeholder="请选择">
+                            <option
+                                    v-for="item in study"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                            </option>
+                        </select>
+                    </div>
+                </div>
+                <div class="user-stu-con">
+                    <div class="userinfo-stu-text">所在行业:</div>
+                    <div class="set-stu-con">
+                        <select  placeholder="请选择">
+                            <option
+                                    v-for="item in study"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                            </option>
+                        </select>
+                    </div>
+                </div>
+                <div class="userinfo-snum-con">
+                    <div class="userinfo-snum-text">身份证号:</div>
+                    <input type="text" class="userinfo-snum-input" :value="snum">
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import dataselect from "../../components/dataselect/dataselect";
     export default {
         name: "userInfo",
+        components:{dataselect},
         data() {
             return {
-                name:'王**'
+                name:'王**',
+                radio:'3',
+                study:['博士','研究生','本科','大专'],
+                snum:'511036...'
             }
         },
         methods: {
@@ -116,6 +176,7 @@
             }
             .userinfo-name-con{
                 clear: both;
+                padding-top: 24rem;
                 .userinfo-name-text{
                     font-size: 14rem;
                     font-family: Source Han Sans CN;
@@ -127,6 +188,124 @@
                 }
                 .userinfo-name-input{
                     width: 160rem;
+                    height: 38rem;
+                    background: #FFFFFF;
+                    border: 1rem solid #E4E7ED;
+                    border-radius: 2rem;
+                    font-size: 14rem;
+                    font-family: Source Han Sans CN;
+                    font-weight: 400;
+                    color: #444444;
+                    float: left;
+                }
+            }
+            .user-sex-con{
+                clear: both;
+                text-align: left;
+                padding-top: 24rem;
+                .user-sex-text{
+                    font-size: 14rem;
+                    font-family: Source Han Sans CN;
+                    font-weight: 400;
+                    color: #444444;
+                    line-height: 38rem;
+                    float: left;
+                    margin-right: 20rem;
+                }
+                .user-sex-choice{
+                    line-height: 38rem;
+                    .user-sex-item{
+                        margin-right: 25rem;
+                        font-size: 14rem;
+                        font-family: Source Han Sans CN;
+                        font-weight: 400;
+                        color: #444444;
+                    }
+                }
+            }
+            .user-bri-con{
+                clear: both;
+                margin-top: 24rem;
+                .userinfo-bri-text{
+                    font-size: 14rem;
+                    font-family: Source Han Sans CN;
+                    font-weight: 400;
+                    color: #444444;
+                    line-height: 38rem;
+                    float: left;
+                    margin-right: 20rem;
+                }
+                .set-data-con{
+                    width: 100rem;
+                    height: auto;
+                    float: left;
+                }
+            }
+            .user-stu-con{
+                clear: both;
+                padding-top: 24rem;
+                .userinfo-stu-text{
+                    font-size: 14rem;
+                    font-family: Source Han Sans CN;
+                    font-weight: 400;
+                    color: #444444;
+                    line-height: 38rem;
+                    float: left;
+                    margin-right: 20rem;
+                }
+                .set-stu-con{
+                        width: 100rem;
+                        height: 38rem;
+                        background: #FFFFFF;
+                        border: 1rem solid #E4E7ED;
+                        border-radius: 2rem;
+                        float: left;
+                        margin-right: 5rem;
+                        line-height: 38rem;
+                        select{
+                            /*清除select的边框样式*/
+                            border: none;
+                            /*清除select聚焦时候的边框颜色*/
+                            outline: none;
+                            /*将select的宽高等于div的宽高*/
+                            width: 100%;
+                            height: 38rem;
+                            line-height: 40rem;
+                            /*隐藏select的下拉图标*/
+                            appearance: none;
+                            -webkit-appearance: none;
+                            -moz-appearance: none;
+                            /*通过padding-left的值让文字居中*/
+                            padding-left: 20rem;
+                        }
+                        .chooce-year:after{
+                            content: "";
+                            width: 14rem;
+                            height: 8rem;
+                            /*background: url(../assets/arrow-down.png) no-repeat center;*/
+                            /*通过定位将图标放在合适的位置*/
+                            position: absolute;
+                            right: 20rem;
+                            top: 41%;
+                            /*给自定义的图标实现点击下来功能*/
+                            pointer-events: none;
+                        }
+                    }
+            }
+            .userinfo-snum-con{
+                clear: both;
+                padding-top: 24rem;
+                .userinfo-snum-text{
+                    font-size: 14rem;
+                    font-family: Source Han Sans CN;
+                    font-weight: 400;
+                    color: #444444;
+                    line-height: 38rem;
+                    float: left;
+                    margin-right: 20rem;
+                }
+                .userinfo-snum-input{
+                    width: 200rem;
                     height: 38rem;
                     background: #FFFFFF;
                     border: 1rem solid #E4E7ED;
