caeb6b0bd44a0de73c8e439ca7dde43edc7fe998
wu <1191125750@qq.com>
2021-03-02 15:25:05 +0800

    change

---
+++ src/views/mobile/withdraw.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="withdraw">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "withdraw"
+    }
+</script>
+
+<style scoped lang="less">
+    .withdraw {
+
+    }
+</style>
