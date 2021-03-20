a9ec34ae3b448e68fb0b0a6c35d61d1db927777f
zhangtao5298 <18280092045@163.com>
2021-03-20 17:09:48 +0800

    cahneg

--- src/components/bottom/bottom.vue
+++ src/components/bottom/bottom.vue
@@ -1,42 +1,53 @@
 <template>
     <div id="bottom">
-        <div class="bottom">
-            <div class="content1">
-                <div class="content">
-                    <div class="left-text1">联系地址</div>
-                    <div class="left-text2">San Frandgcc CA 78567</div>
-                    <div class="left-text3">地址：大连市沙河口区中山路688号</div>
-                </div>
-                <div class="content">
-                    <div class="left-text1">联系方式</div>
-                    <div class="left-text2">18535336852@qq.com</div>
-                    <div class="left-text3">Hhttps://lanhuapp.com/..</div>
-                </div>
-                <div  class="content">
-                    <div class="left-text1">通讯方式</div>
-                    <div class="img-content">
-                        <img :src=facebook class="facebook">
-                        <img :src=ins class="ins">
-                        <img :src=twitter class="twitter">
+        <div class="section11" :style="{ 'backgroundImage':'url('+ section3Bg +')' }">
+                    <div class="section11-wrap">
+                        <!-- <div class="section11-wrap-title">
+                            {{ $t('index').join }}
+                        </div>
+                        <div class="section11-wrap-tip">
+                            {{ $t('index').joinText }}
+                        </div> -->
+                        <!-- <div class="section11-wrap-btnBox">
+                            <div class="input-item">
+                                <input type="text" :placeholder="$t('index').namePlaceholder ">
+                            </div>
+                            <div class="input-item">
+                                <input type="email" :placeholder="$t('index').emailPlaceholder ">
+                            </div>
+                            <div class="btn">
+                                {{ $t('index').submit }}
+                            </div>
+                        </div> -->
+                        <div class="url">
+                            <div class="url-content">{{ $t('index').net1[0] }}<span>{{ $t('index').net1[1] }}</span></div>
+                            <div class="url-content">{{ $t('index').net2[0] }}<span>{{ $t('index').net2[1] }}</span></div>
+                            <div class="url-content email">{{ $t('index').net3[0] }}</div>
+                        </div>
+                        <div class="logo-icon">
+                            <img :src="logoIcon" alt="" class="icon-img">
+                        </div>
+                        <div class="foot-text">© {{ $t('index').own }}<strong>
+
+                        </strong></div>
                     </div>
                 </div>
-            </div>
-            <div class="line"></div>
-            <div class="content2">
-                © © Smart Learning Technology Global 2020. All rights reserved.
-            </div>
-        </div>
+                <!-- <TopBar></TopBar> -->
+                
     </div>
 </template>
 
 <script>
+// import TopBar from "../../components/header/topBar";
     export default {
         name: "bottom",
+        // components: {TopBar},
         data(){
             return{
                 facebook:`${require('../../static/img/bottom/facebook.png')}`,
                 ins:`${require('../../static/img/bottom/ins.png')}`,
-                twitter:`${require('../../static/img/bottom/twitter.png')}`
+                twitter:`${require('../../static/img/bottom/twitter.png')}`,
+                logoIcon: `${require('../../static/img/index/sltlogo_60x120_white.png')}`,
             }
         }
     }
@@ -47,6 +58,131 @@
     // position: fixed;
     // bottom: 0;
     // z-index: 20;
+    .section11 {
+                height: 380zrem;
+                background-color: #040622;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+
+                .section11-wrap {
+                    width: 1140rem;
+                    margin: 0 auto;
+                    height: 100%;
+                    padding: 80rem 0 60rem 0;
+                    box-sizing: border-box;
+
+                    .section11-wrap-title {
+
+                        color: #fff;
+                        font-weight: 400;
+                        font-style: normal;
+                        opacity: 1;
+                        visibility: visible;
+                        margin-bottom: 15rem;
+                        line-height: 1.3em;
+                        font-size: 36rem;
+                    }
+
+                    .section11-wrap-tip {
+                        font-size: 20rem;
+                        line-height: 1.68421053em;
+                        font-weight: 400;
+                        font-style: normal;
+                        color: #89c1f9;
+                        margin-bottom: 40rem;
+                    }
+
+                    .section11-wrap-btnBox {
+                        width: 798rem;
+                        height: auto;
+                        box-sizing: border-box;
+                        margin: 0 auto;
+                        display: flex;
+                        margin-bottom: 60rem;
+
+                        .input-item {
+                            padding: 0 15rem;
+                            box-sizing: border-box;
+
+                            input {
+                                padding: 0 20rem;
+                                font-style: normal;
+                                font-weight: 300;
+                                font-size: 16rem;
+                                display: block;
+                                border: 0;
+                                border-bottom: 1rem solid #d0d0d0;
+                                background: #fff;
+                                color: #010119;
+                                line-height: 1;
+                                box-sizing: border-box;
+                                min-height: 46.5rem;
+                                transition: border .25s;
+                                width: 246rem;
+                                height: 46.5rem;
+                                border-radius: 999rem;
+                            }
+                        }
+
+                        .btn {
+                            width: 246rem;
+                            height: 46.5rem;
+                            background: #1622a7;
+                            line-height: 46.5rem;
+                            color: #fff;
+                            position: relative;
+                            border-radius: 999rem;
+                            font-size: 12rem;
+                            cursor: pointer;
+                        }
+
+                    }
+
+                    .url {
+                        color: #fff;
+                        font-size: 16rem;
+                        line-height: 1.6em;
+                        margin-bottom: 35rem;
+
+                        .url-content {
+                            font-weight: 700;
+
+                            span {
+                                font-weight: 400;
+                                font-style: normal;
+                                margin-left: 15rem;
+                                cursor: pointer;
+                            }
+                        }
+
+                        .email {
+                            margin-top: 35rem;
+                            color: #ffff00;
+                        }
+                    }
+
+                    .logo-icon {
+                        margin-bottom: 35rem;
+
+                        .icon-img {
+                            width: 120rem;
+                            height: 60rem;
+                        }
+                    }
+
+                    .foot-text {
+                        font-weight: 400;
+                        font-style: normal;
+                        color: #89c1f9;
+                        font-size: 16rem;
+                        opacity: 1;
+                        visibility: visible;
+                        line-height: 1.6em;
+                    }
+
+                }
+            }
     .bottom{
         width: 1920rem;
         height: 200rem;
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -53,13 +53,13 @@
                                     <h4>{{ $t('index').whitBookT }}</h4>
                                 </div>
                             </div> -->
-                            <div class="videoBox">
+                            <!-- <div class="videoBox">
                                  <video-player  class="video-player vjs-custom-skin"
                                 ref="videoPlayer"
                                 :playsinline="true"
                                 :options="playerOptions"
                             ></video-player>
-                            </div>
+                            </div> -->
                         </div>
                         <div class="ico">
                             <div class="ico-content">
@@ -934,7 +934,7 @@ export default {
             // 视频播放
             playerOptions: {
                 playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
-                autoplay: false, //如果true,浏览器准备好时开始回放。
+                autoplay: true, //如果true,浏览器准备好时开始回放。
                 muted: false, // 默认情况下将会消除任何音频。
                 loop: false, // 导致视频一结束就重新开始。
                 preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
