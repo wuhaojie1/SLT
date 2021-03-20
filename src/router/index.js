429dc7142419db85c8a602a296f16cbf919cb491
zhangtao5298 <18280092045@163.com>
2021-03-20 15:21:54 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,18 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [{
-        path: '/',
-        name: 'login',
-        component: () =>
-            import ('../views/login/login.vue')
-    },
+const routes = [
     {
-        path: '/index',
+        path: '/',
         name: 'index',
         component: () =>
             import ('../views/index/index.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () =>
+            import ('../views/login/login.vue')
     },
     {
         path: '/regiest',
